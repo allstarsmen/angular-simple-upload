f8277de69a6558addb7add04ee79b472594aa28f
Nam Pham <nam@intellifinder.dk>
2023-04-02 09:27:25 +0700

    add SCSS file

--- projects/ngx-simple-upload/src/lib/simple-upload.component.ts
+++ projects/ngx-simple-upload/src/lib/simple-upload.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lib-ngx-simple-upload',
   templateUrl: './simple-upload.component.html',
-  styles: [
-  ]
+  styleUrls: ['./simple-upload.component.scss']
 })
 export class SimpleUploadComponent {
 
